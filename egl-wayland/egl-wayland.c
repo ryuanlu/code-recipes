31a01a1152271603073f27cbe6d7610425365119
盧瑞元 (Ruei-Yuan Lu) <RueiYuan.Lu@gmail.com>
2019-03-04 01:06:09 +0800

    Support window resizing for egl-wayland (not perfect)

--- egl-wayland/egl-wayland.c
+++ egl-wayland/egl-wayland.c
@@ -15,6 +15,9 @@
 
 #define eglGetPlatformDisplayEXT	eglGetPlatformDisplay
 
+#define WINDOW_WIDTH	(640)
+#define WINDOW_HEIGHT	(360)
+
 struct W_context
 {
 	struct wl_compositor*		compositor;
@@ -27,7 +30,13 @@ struct W_context
 	struct wl_egl_window*		window;
 	struct wl_seat*			seat;
 	struct wl_pointer*		pointer;
-	int				x, y;
+	int				width;
+	int				height;
+
+	EGLDisplay			EGL_display;
+	EGLSurface			draw_surface;
+	EGLContext			EGL_context;
+	EGLConfig			config;
 };
 
 void wl_registry_global(void *data, struct wl_registry *wl_registry, uint32_t name, const char *interface, uint32_t version)
@@ -56,11 +65,46 @@ static void xdg_shell_ping(void* data, struct zxdg_shell_v6* xdg_shell, uint32_t
 
 static void xdg_surface_configure(void* data, struct zxdg_surface_v6* surface, uint32_t serial)
 {
+	struct W_context* WL = (struct W_context*)data;
 	zxdg_surface_v6_ack_configure(surface, serial);
+	glClearColor (0.0, 1.0, 0.0, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+	eglSwapBuffers(WL->EGL_display, WL->draw_surface);
 }
 
 static void xdg_toplevel_configure(void* data, struct zxdg_toplevel_v6* toplevel, int32_t width, int32_t height, struct wl_array* states)
 {
+	struct W_context* WL = (struct W_context*)data;
+	uint32_t *ps;
+
+	printf("toplevel-configure: w %d, h %d / states: ", width, height);
+
+	wl_array_for_each(ps, states)
+	{
+		switch(*ps)
+		{
+		case ZXDG_TOPLEVEL_V6_STATE_MAXIMIZED:
+			printf("MAXIMIZED ");
+			break;
+		case ZXDG_TOPLEVEL_V6_STATE_FULLSCREEN:
+			printf("FULLSCREEN ");
+			break;
+		case ZXDG_TOPLEVEL_V6_STATE_RESIZING:
+			printf("RESIZING %d, %d ", WL->width, WL->height);
+			eglMakeCurrent(WL->EGL_display, EGL_NO_SURFACE, EGL_NO_SURFACE, WL->EGL_context);
+			eglDestroySurface(WL->EGL_display, WL->draw_surface);
+			wl_egl_window_destroy(WL->window);
+			WL->window = wl_egl_window_create(WL->surface, WL->width + width, WL->height + height);
+			WL->draw_surface = eglCreatePlatformWindowSurfaceEXT(WL->EGL_display, WL->config, WL->window, NULL);
+			eglMakeCurrent(WL->EGL_display, WL->draw_surface, WL->draw_surface, WL->EGL_context);
+			break;
+		case ZXDG_TOPLEVEL_V6_STATE_ACTIVATED:
+			printf("ACTIVATED ");
+			break;
+		}
+	}
+
+	printf("\n");
 }
 
 static void xdg_toplevel_close(void* data, struct zxdg_toplevel_v6* toplevel)
@@ -69,19 +113,14 @@ static void xdg_toplevel_close(void* data, struct zxdg_toplevel_v6* toplevel)
 
 void wl_pointer_enter(void *data, struct wl_pointer *wl_pointer, uint32_t serial, struct wl_surface *surface, wl_fixed_t surface_x, wl_fixed_t surface_y)
 {
-	struct W_context* WL = (struct W_context*)data;
-	WL->x = wl_fixed_to_double(surface_x);
-	WL->y = wl_fixed_to_double(surface_y);
 }
 
-void wl_pointer_leave(void *data, struct wl_pointer *wl_pointer, uint32_t serial, struct wl_surface *surface) {}
+void wl_pointer_leave(void *data, struct wl_pointer *wl_pointer, uint32_t serial, struct wl_surface *surface)
+{
+}
 
 void wl_pointer_motion(void *data, struct wl_pointer *wl_pointer, uint32_t time, wl_fixed_t surface_x, wl_fixed_t surface_y)
 {
-	struct W_context* WL = (struct W_context*)data;
-	fprintf(stderr, "%f, %f\n", wl_fixed_to_double(surface_x) - WL->x, wl_fixed_to_double(surface_y) - WL->y);
-	WL->x = wl_fixed_to_double(surface_x);
-	WL->y = wl_fixed_to_double(surface_y);
 }
 
 void wl_pointer_button(void *data, struct wl_pointer *wl_pointer, uint32_t serial, uint32_t time, uint32_t button, uint32_t state)
@@ -91,6 +130,12 @@ void wl_pointer_button(void *data, struct wl_pointer *wl_pointer, uint32_t seria
 	{
 		fprintf(stderr, "Move\n");
 		zxdg_toplevel_v6_move(WL->xdg_toplevel, WL->seat, serial);
+	}
+
+	if(button == BTN_RIGHT && state == WL_POINTER_BUTTON_STATE_PRESSED)
+	{
+		fprintf(stderr, "resize\n");
+		zxdg_toplevel_v6_resize(WL->xdg_toplevel, WL->seat, serial, ZXDG_TOPLEVEL_V6_RESIZE_EDGE_BOTTOM_RIGHT);
 	}
 }
 
@@ -141,11 +186,7 @@ int main(int argc, char const *argv[])
 		.axis_discrete = NULL
 	};
 
-	EGLDisplay	EGL_display = NULL;
-	EGLSurface	draw_surface = NULL;
-	EGLContext	EGL_context = NULL;
 	int nr_configs;
-	EGLConfig	config;
 
 	EGLint config_attributes[] =
 	{
@@ -178,24 +219,22 @@ int main(int argc, char const *argv[])
 
 	wl_surface_commit(WL->surface);
 
-	WL->window = wl_egl_window_create (WL->surface, 640, 360);
 	wl_pointer_add_listener(WL->pointer, &pointer_listener, WL);
 
-	EGL_display = eglGetPlatformDisplayEXT(EGL_PLATFORM_WAYLAND_EXT, W_display, NULL);
-	eglInitialize(EGL_display, NULL, NULL);
+	WL->EGL_display = eglGetPlatformDisplayEXT(EGL_PLATFORM_WAYLAND_EXT, W_display, NULL);
+	eglInitialize(WL->EGL_display, NULL, NULL);
 	eglBindAPI(EGL_OPENGL_API);
-	eglChooseConfig(EGL_display, config_attributes, &config, 1, &nr_configs);
-	draw_surface = eglCreatePlatformWindowSurfaceEXT(EGL_display, config, WL->window, NULL);
-	EGL_context = eglCreateContext(EGL_display, config, EGL_NO_CONTEXT, context_attributes);
-	eglMakeCurrent(EGL_display, draw_surface, draw_surface, EGL_context);
+	eglChooseConfig(WL->EGL_display, config_attributes, &WL->config, 1, &nr_configs);
+	WL->EGL_context = eglCreateContext(WL->EGL_display, WL->config, EGL_NO_CONTEXT, context_attributes);
+
+	WL->window = wl_egl_window_create (WL->surface, WINDOW_WIDTH, WINDOW_HEIGHT);
+	WL->width = WINDOW_WIDTH;
+	WL->height = WINDOW_HEIGHT;
+	WL->draw_surface = eglCreatePlatformWindowSurfaceEXT(WL->EGL_display, WL->config, WL->window, NULL);
+	eglMakeCurrent(WL->EGL_display, WL->draw_surface, WL->draw_surface, WL->EGL_context);
 
 	while(running)
-	{
-		wl_display_dispatch_pending(W_display);
-		glClearColor (0.0, 1.0, 0.0, 1.0);
-		glClear(GL_COLOR_BUFFER_BIT);
-		eglSwapBuffers(EGL_display, draw_surface);
-	}
+		wl_display_dispatch(W_display);
 
 	wl_registry_destroy(W_registry);
 	wl_display_disconnect(W_display);
