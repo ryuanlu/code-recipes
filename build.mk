82f4a881543e8866707ad17a2f4ef90bb391b545
盧瑞元 (Ruei-Yuan Lu) <RueiYuan.Lu@gmail.com>
2019-11-03 22:41:56 +0800

    Adopt new Makefile build system

---
+++ build.mk
@@ -0,0 +1,19 @@
+define define_c_target
+$(1)_obj_files:=$$(foreach obj,$$($(1)_src_files:.c=.o),$$(obj))
+
+$$($(1)_obj_files): %.o: %.c
+	@echo "\tCC\t$$@"
+	$$(CC) -c $$< -o $$@ $$($(1)_cflags)
+
+$(1): $$($(1)_obj_files)
+	@echo "\tLD\t$$@"
+	$$(CC) $$^ -o $$@ $$($(1)_ldflags)
+
+$(1).d: $$(foreach src,$$($(1)_src_files),$$(src))
+	@echo "\tDEP\t$$@"
+	$$(CC) -MM $$^ > $$@
+
+$(1)_clean:
+	@echo "\tCLEAN\t$(1)"
+	rm -f $$($(1)_obj_files) $(1) $(1).d
+endef
